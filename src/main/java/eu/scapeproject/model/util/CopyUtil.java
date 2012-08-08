75be3cbd98f52a7a788602e55a68de534c75dafa
fasseg <frank.asseg@googlemail.com>
2012-08-08 12:32:14 +0200

    added deepcopy util

--- src/main/java/eu/scapeproject/model/util/CopyUtil.java
+++ src/main/java/eu/scapeproject/model/util/CopyUtil.java
@@ -2,14 +2,34 @@ package eu.scapeproject.model.util;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.Member;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-import com.sun.xml.bind.v2.schemagen.xmlschema.List;
 
 public abstract class CopyUtil {
-    public static <T> T deepCopy(Class<?> type, T obj) throws Exception {
+    @SuppressWarnings("unchecked")
+	public static <T> T deepCopy(Class<?> type, T obj) throws Exception {
+        if (type == String.class){
+			return (T) new String((String) obj);
+        } else if (type == int.class  || type == Integer.class){
+        	return (T) (Integer) obj;
+        } else if (type == long.class || type == Long.class){
+        	return (T) (Long) obj;
+        } else if (type == Date.class){
+        	return (T) new Date(((Date) obj).getTime());
+        } else if (obj instanceof List){
+        	List<Object> origList=(List<Object>) obj;
+        	if (origList != null){
+        		List<Object> newList=new ArrayList<Object>(origList.size());
+        		for (Object element:origList){
+        			newList.add(deepCopy(element.getClass(), element));
+        		}
+        		return (T) newList;
+        	}
+        }
+        
         T copy = null;
         for (Constructor<?> c : type.getDeclaredConstructors()) {
             if (c.getParameterTypes().length == 0 && Modifier.isPrivate(c.getModifiers())) {
@@ -19,24 +39,11 @@ public abstract class CopyUtil {
         }
         for (Field f : obj.getClass().getDeclaredFields()) {
             f.setAccessible(true);
-            if (f.getType() == String.class) {
-                String val = (String) f.get(obj);
-                f.set(copy, val);
-            } else if (f.getType() == int.class) {
-                if (f.get(obj) != null) {
-                    f.set(copy, f.get(obj));
-                }
-            } else if (f.getType() == long.class) {
-                if (f.get(obj) != null) {
-                    f.set(copy, f.get(obj));
-                }
-            } else if (f.getType() == List.class) {
-                System.out.println("skipping list");
-            } else {
-                if (f.get(obj) != null){
-                    System.out.println("recuring into " + f.getName() + " with type " + f.getType().getName());
-                    f.set(copy, deepCopy(f.getType(),f.get(obj)));
-                }
+            if (!Modifier.isStatic(f.getModifiers())){
+	            Object val=f.get(obj);
+	            if (val != null){
+	            	f.set(copy,deepCopy(val.getClass(), val));
+	            }
             }
         }
         return copy;
