861dbbeeba6a73f486c7ae24c0f6dcc9a6d2bfb4
fasseg <frank.asseg@googlemail.com>
2012-11-05 11:49:32 +0100

    added convenience method to serilializer

--- src/main/java/eu/scapeproject/model/mets/SCAPEMarshaller.java
+++ src/main/java/eu/scapeproject/model/mets/SCAPEMarshaller.java
@@ -1,5 +1,6 @@
 package eu.scapeproject.model.mets;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
@@ -167,6 +168,12 @@ public class SCAPEMarshaller {
 		return unmarshaller;
 	}
 
+	public String serialize(Object subject) {
+		final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		this.serialize(subject,bos);
+		return bos.toString();
+	}
+	
 	public void serialize(Object subject, OutputStream out) throws SerializationException {
 		try {
 			if (subject instanceof IntellectualEntity) {
