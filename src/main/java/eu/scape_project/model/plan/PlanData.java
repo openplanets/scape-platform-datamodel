2aaae2d2731f1a66afe83077c6587b3885e08fe0
fasseg <frank.asseg@googlemail.com>
2013-12-22 14:54:56 +0100

    added license header to PlanData

--- src/main/java/eu/scape_project/model/plan/PlanData.java
+++ src/main/java/eu/scape_project/model/plan/PlanData.java
@@ -1,5 +1,15 @@
-/**
- *
+/*
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
  */
 
 package eu.scape_project.model.plan;
@@ -15,6 +25,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 import eu.scape_project.model.Identifier;
 import eu.scape_project.model.LifecycleState;
 
+/**
+ * For interaction used by external applications.
+ * @author frank asseg
+ *
+ */
 @XmlRootElement(name = "plan-data", namespace = "http://scape-project.eu/model")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class PlanData {
@@ -110,5 +125,4 @@ public class PlanData {
             return new PlanData(this);
         }
     }
-
 }
