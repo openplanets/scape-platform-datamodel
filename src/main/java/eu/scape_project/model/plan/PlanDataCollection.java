4e25a330c94d0b71c545df031a8dc9325a2c6e06
fasseg <frank.asseg@googlemail.com>
2013-12-27 16:19:09 +0100

    added size field to PlanDataCollection

--- src/main/java/eu/scape_project/model/plan/PlanDataCollection.java
+++ src/main/java/eu/scape_project/model/plan/PlanDataCollection.java
@@ -18,6 +18,7 @@ import java.util.Collection;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -28,18 +29,27 @@ public class PlanDataCollection {
     @XmlElement(name = "plan-data", namespace = "http://scape-project.eu/model")
     private final Collection<PlanData> planData;
 
+    @XmlAttribute(name = "size")
+    private final int size;
+
     public PlanDataCollection(Collection<PlanData> planData) {
         super();
         this.planData = planData;
+        this.size = (planData == null) ? 0 : planData.size();
     }
 
     private PlanDataCollection() {
         super();
         this.planData = null;
+        this.size = 0;
     }
 
     public Collection<PlanData> getPlanData() {
         return planData;
+    }
+
+    public int getSize() {
+        return size;
     }
 
 }
